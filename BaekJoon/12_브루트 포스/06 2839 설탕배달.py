ed3e7a9e64b968867e0adfb42554e47a0ff4f09f
kim <hwiyul_g@daum.net>
2023-09-16 15:28:21 +0900

    0916 : 2839 설탕배달(greedy)

--- BaekJoon/12_브루트 포스/06 2839 설탕배달.py
+++ BaekJoon/12_브루트 포스/06 2839 설탕배달.py
@@ -1,11 +1,12 @@
 n = int(input())
 
-ans = 0
+cnt = 0
 while n >= 0:
     if n % 5 == 0:
-        ans += (n//5)
+        cnt += n // 5
+        print(cnt)
         break
     n -= 3
-    ans += 1
-print(ans) if ans != 0 else print(-1)
-
+    cnt += 1
+else:
+    print(-1)
