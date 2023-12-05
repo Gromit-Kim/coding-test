dbfd22870016766d4d1733ba0337a961b6168296
kim <hwiyul_g@daum.net>
2023-12-05 12:09:41 +0900

    백준 10773 제로

---
+++ BaekJoon/16_스택, 큐, 덱/02 10773 제로.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+stack = []
+for _ in range(k):
+    num = int(input())
+    if num == 0 and stack:
+        stack.pop()
+        continue
+    stack.append(num)
+
+print(sum(stack))
