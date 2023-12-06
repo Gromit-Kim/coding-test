8d7a044768a416a05bc0f8c0e6cec9d2fd28eb5c
kim <hwiyul_g@daum.net>
2023-12-06 15:41:03 +0900

    fix : 속도 개선

--- BaekJoon/16_스택, 큐, 덱/04 4949 균형잡힌 세상.py
+++ BaekJoon/16_스택, 큐, 덱/04 4949 균형잡힌 세상.py
@@ -4,14 +4,21 @@ while True:
         break
 
     stack = []
+    is_balanced = True
     for ch in s:
         if ch == '(' or ch == '[':
             stack.append(ch)
         elif ch == ')':
             if stack and stack[-1] == '(':
                 stack.pop()
+            else:
+                is_balanced = False
+                break
         elif ch == ']':
             if stack and stack[-1] == '[':
                 stack.pop()
+            else:
+                is_balanced = False
+                break
     
-    print("yes") if not stack else print("no")
+    print("yes") if not stack and is_balanced else print("no")
