252947eff6f30017e1f34fa850fbe04b952860ab
kim <hwiyul_g@daum.net>
2024-01-03 18:04:41 +0900

    ?

--- BaekJoon/구현/2979 트럭 주차.py
+++ BaekJoon/구현/2979 트럭 주차.py
@@ -1,8 +1,23 @@
-prices = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-pay = 0
-for price in prices:
+a, b, c = map(int, input().split())
+car_count_by_time = dict()
+for _ in range(3):
     start, end = map(int, input().split())
-    pay += price * (end - start)
+    for i in range(start, end):
+        if i in car_count_by_time:
+            car_count_by_time[i] += 1
+        else:
+            car_count_by_time[i] = 1
 
-print(pay)
+ans = 0
+for val in car_count_by_time.values():
+    if val == 1:
+        ans += a
+    elif val == 2:
+        ans += b * 2
+    elif val == 3:
+        ans += c * 3
+
+print(ans)
