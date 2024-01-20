c71ebfb34efc5abaf2c4158db9d85221cfbd2f75
kim <hwiyul_g@daum.net>
2024-01-21 00:21:52 +0900

    02 조이스틱 - 그리디

--- Programmers/알고리즘 고득점 Kit/greedy/02 조이스틱.py
+++ Programmers/알고리즘 고득점 Kit/greedy/02 조이스틱.py
@@ -1,22 +1,16 @@
 def move_count(ch):
-    if ch == 'M':
-        return 13
-    elif ch < 'M':
-        return ord(ch) - ord('A')
-    else:
-        return ord('Z') - ord(ch) + 1
+    return min(ord(ch) - ord('A'), ord('Z') - ord(ch) + 1)
 
 def solution(name):
     answer = 0
     n = len(name)
-    min_move = n - 1
+    min_move = n - 1 # 한쪽으로 끝까지 이동하기 (최대 이동 횟수)
     for i in range(n):
         answer += move_count(name[i])
-        
-        nc = i + 1
-        while nc < n and name[nc] == 'A':
-            nc += 1
-        min_move = min(min_move, i*2 + n - nc)
-        
-    answer += min_move
-    return answer
+
+        next_ch = i + 1
+        while next_ch < n and name[next_ch] == 'A':
+            next_ch += 1 # A가 아닌 문자가 나올 때까지 이동
+        min_move = min(min_move, i + i + n - next_ch) # i까지 이동 후, 왼쪽으로가서 next_ch까지 이동
+        min_move = min(min_move, i + 2 *(n - next_ch)) # i까지 이동 후, 오른쪽으로 next_ch까지 이동
+    return answer + min_move
