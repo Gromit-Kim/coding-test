be57ca46aa9b3962a5059b31f680733ed04aa3e2
kim <hwiyul_g@daum.net>
2024-01-22 12:01:29 +0900

    시간초과 해결

--- Programmers/알고리즘 고득점 Kit/greedy/03 큰 수 만들기 - 1.py
+++ Programmers/알고리즘 고득점 Kit/greedy/03 큰 수 만들기 - 1.py
@@ -4,10 +4,12 @@ def solution(number, k):
     start = 0
     for i in range(n-k, 0, -1):
         end = n - i
-        max_n = 0
+        max_n = '0'
         for j in range(start, end+1):
-            if max_n < int(number[j]):
-                max_n = int(number[j])
+            if max_n < number[j]:
+                max_n = number[j]
                 start = j + 1
-        answer += str(max_n)
+            if max_n == '9':
+                break
+        answer += max_n
     return answer
