bab1936acf45c83af802b5c1e666d73a5bc8ef20
kim <hwiyul_g@daum.net>
2024-01-25 12:10:37 +0900

    topdown - 재귀

---
+++ BaekJoon/DP/2579 계단 오르기 - topDown copy.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def find_max_score(i, steps, dp):
+    if i <= 0:
+        return 0
+    if dp[i] != -1:
+        return dp[i]
+    dp[i] = max(find_max_score(i-2, steps, dp) + steps[i], 
+                find_max_score(i-3, steps, dp) + steps[i-1] + steps[i])
+    
+t = int(input())
+steps = [int(input()) for _ in range(t)]
+dp = [-1] * (t+1)
+find_max_score(t, steps, dp)
+print(dp[t])
