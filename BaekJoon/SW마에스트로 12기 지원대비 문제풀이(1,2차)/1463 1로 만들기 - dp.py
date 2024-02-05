4730787779297658c5074f23f2f66b753e47c654
kim <hwiyul_g@daum.net>
2024-02-05 23:12:07 +0900

    1463 dp - IndexError 해결, n이 2나 3이 주어졌을 때 문제가 발생한 것으로 추측

--- BaekJoon/SW마에스트로 12기 지원대비 문제풀이(1,2차)/1463 1로 만들기 - dp.py
+++ BaekJoon/SW마에스트로 12기 지원대비 문제풀이(1,2차)/1463 1로 만들기 - dp.py
@@ -1,8 +1,6 @@
 n = int(input())
 dp = [0] * (n+1)
-dp[2] = 1
-dp[3] = 1
-for i in range(4, n+1):
+for i in range(2, n+1):
     dp[i] = dp[i-1] + 1
     if i % 2 == 0:
         dp[i] = min(dp[i], dp[i//2]+1)
