b6511d5d7351582d83b8a2bbd8b53540dccc6fe2
kim <hwiyul_g@daum.net>
2024-03-07 15:01:39 +0900

    연습문제1 구현

---
+++ python/이코테/ch 04 구현/연습문제 1 상하좌우.py
@@ -0,0 +1,15 @@
+dic = {'U' : 0, 'D': 1, 'L' : 4, 'R' : 3}
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+n = int(input())
+plans = input().split()
+
+x, y = 1, 1 #  현재
+for plan in plans:
+    i = dic[plan]
+    nx, ny = x +dx[i] , y+ dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
+print(x, y)
