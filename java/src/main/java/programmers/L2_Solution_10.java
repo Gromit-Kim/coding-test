18c42bbbad3d1729d052668b9e8d386b3d1169bb
kim <hwiyul_g@daum.net>
2024-05-23 13:19:29 +0900

    프로그래머스 L2 완전탐색 카펫

---
+++ java/src/main/java/programmers/L2_Solution_10.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class L2_Solution_10 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int total = brown + yellow;
+        for (int h = 1; h * h <= total; h++) {
+            if (total % h == 0) {
+                int w = total / h;
+                if (2 * (w + h - 2) == brown) {
+                    answer[0] = w;
+                    answer[1] = h;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
