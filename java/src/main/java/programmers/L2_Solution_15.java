850581787e90a080328f7edd59269334a7a8c5c3
kim <hwiyul_g@daum.net>
2024-05-24 15:53:14 +0900

    프로그래머스 L2 멀리 뛰기

---
+++ java/src/main/java/programmers/L2_Solution_15.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class L2_Solution_15 {
+    private static long[] dp;
+    public long solution(int n) {
+        dp = new long[n+1]; // i 칸에 도달하는 방법의 수
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i-1] + dp[i-2])%1234567;
+        }
+        return dp[n];
+    }
+}
