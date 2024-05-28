42a1973630a33700ffb6e1d59def083c48599d72
kim <hwiyul_g@daum.net>
2024-05-28 16:17:58 +0900

    프로그래머스 L2 의상 - 해시

---
+++ java/src/main/java/programmers/L2_Solution_24.java
@@ -0,0 +1,18 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class L2_Solution_24 {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] c : clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+        }
+        int answer = 1;
+        for (String key : map.keySet()) {
+            answer *= (map.get(key) + 1);
+        }
+        return answer - 1;
+    }
+}
