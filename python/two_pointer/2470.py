c79c735912226cd3615b06940a1d3cee111ad75d
KSY <sye.g112@gmail.com>
2025-07-24 09:42:53 +0900

    two pointer

--- python/two_pointer/2470.py
+++ python/two_pointer/2470.py
@@ -7,14 +7,17 @@ result = []
 left, right = 0, n-1
 while(left < right):
     total = arr[left] + arr[right]
+
     if abs(total) < diff:
         diff = abs(total)
         result = [arr[left], arr[right]]
     
     if total < 0:
         left += 1
-    else:
+    elif total > 0:
         right -= 1
+    else:
+        break
 
 print(*sorted(result))
 
